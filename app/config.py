26a3534a87f714567081fff9b8058c90cb78b0ec
Tuan Pham <tuanqpham0921@gmail.com>
2025-08-02 11:02:53 -0500

    remove dotenv

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH", "./data/chroma_db")
