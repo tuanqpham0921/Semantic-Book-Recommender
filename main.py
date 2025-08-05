3bd06cc50b23bdae0f418c8775c4e8a039bcfe3b
Tuan Pham <tuanqpham0921@gmail.com>
2025-08-05 11:46:40 -0500

    working filters

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class BookRecommendation(BaseModel):
     isbn13: str = Field(default="")
     title: str = Field(default="")
     authors: str = Field(default="")
-    categories: str = Field(default="")
+    categories: Optional[str] = Field(default=None)
     thumbnail: str = Field(default="")
     description: str = Field(default="")
     published_year: Optional[int] = Field(default=None)
@@ -67,28 +67,59 @@ class BookRecommendation(BaseModel):
     class Config:
         extra = 'allow'  # Allow extra fields if necessary
 
-# Performs a semantic search using the ChromaDB to retrieve book recommendations
-# based on the input query.
+# Performs a semantic search using the ChromaDB 
+# to retrieve book recommendations based on the input query.
 def retrieve_semantic_recommendations(
         query: str,
-        initial_top_k: int = 10,
+        category: str = "All",
+        tone: str = "Happy",
+        max_pages: Optional[int] = None,
+        initial_top_k: int = 50,
+        final_top_k: int = 10
 ) -> pd.DataFrame:
     recs = db_books.similarity_search(query, k=initial_top_k)
     books_list = [rec.page_content.strip('"').split()[0] for rec in recs]
     book_recs = books[books["isbn13"].isin(books_list)].head(initial_top_k)
-    return book_recs
+
+    # filter out the max number of pages if provided
+    if max_pages is not None:
+        book_recs = book_recs[book_recs["num_pages"] <= max_pages]
+
+    # if category is not "All", filter by category
+    if category != "All":
+        book_recs = book_recs[book_recs["simple_categories"] == category]
+
+    # only get the top recommendations based on the tone
+    if tone == "Happy":
+        book_recs.sort_values(by="joy", ascending=False, inplace=True)
+    elif tone == "Surprising":
+        book_recs.sort_values(by="surprise", ascending=False, inplace=True)
+    elif tone == "Angry":
+        book_recs.sort_values(by="anger", ascending=False, inplace=True)
+    elif tone == "Suspenseful":
+        book_recs.sort_values(by="fear", ascending=False, inplace=True)
+    elif tone == "Sad":
+        book_recs.sort_values(by="sadness", ascending=False, inplace=True)
+
+    # get the top ten recommendations
+    return book_recs.head(final_top_k)
+    
 
 # Endpoint to recommend books based on user query
 @app.post("/recommend_books", response_model=List[BookRecommendation])
 def recommend_books(request: RecommendationRequest):
     # Embed user query
-    df = retrieve_semantic_recommendations(request.description)
-    
+    df = retrieve_semantic_recommendations(request.description,
+                                           request.filters["category"],
+                                           request.filters["tone"],
+                                           request.filters.get("max_pages", None))
+
     # Convert DataFrame rows to BookRecommendation objects
     recommendations = [
         BookRecommendation(**row.to_dict())
         for _, row in df.iterrows()
     ]
+
     return recommendations
 
 # place holder for API root endpoint
