26b2132ef1a72f8117c23ee6d36c0860b21b03e8
Tuan Pham <tuanqpham0921@gmail.com>
2025-08-13 18:07:31 -0500

    seperated filter and content

--- test_api.py
+++ test_api.py
@@ -1,34 +1,18 @@
 import requests
 import sys
 
+recommend_books_url = "http://localhost:8000/recommend_books"
+reason_query_url    = "http://localhost:8000/reason_query"
 
-url = "http://localhost:8000/recommend_books"
 headers = {"Content-Type": "application/json"}
+prompts = [
+    "a book about forgiveness",
+    "a 300 page or more books by Stephen King with a sad tone and take place in Maine",
+    "get me books by Stephen King and J.K. Rowling"
+]
 #===================================================================
 
-def main():
-    prompts = [
-        "a book about forgiveness",
-        "a 300 page or more books by Stephen King with a sad tone and take place in Maine",
-        "get me books by Stephen King and J.K. Rowling"
-    ]
-
-    # If a query is passed as an argument, use it; otherwise use default
-    if len(sys.argv) > 1:
-        query = " ".join(sys.argv[1:])
-    else:
-        query = prompts[0]
-
-    payload = {"description": query}
-    response = requests.post(url, json=payload, headers=headers)
-
-    # there is nothing here so we just quit
-    if not response.ok:
-        print(f"Error {response.status_code}: {response.text}")
-        quit()
-
-    result = response.json()
-
+def print_result(result, query):
     print("=" * 110)
     print("QUERY: \n")
     print(query)
@@ -56,14 +40,14 @@ def main():
     # Print all unique authors (one-liner)
     authors_set = set([author.strip() for book in result for author in str(book['authors']).replace(';', ',').split(',') if author.strip()])
     print("Unique Authors: \n")
-    print(", ".join(sorted(authors_set)))
+    print("\n".join(sorted(authors_set)))
     print("-" * 110)
 
 
     # Print all unique genres (one-liner)
     genres_set = set([genre.strip() for book in result for genre in str(book['simple_categories']).replace(';', ',').split(',') if genre.strip()])
     print("Unique Genres: \n")
-    print(", ".join(sorted(genres_set)))
+    print("\n".join(sorted(genres_set)))
     print("-" * 110)
 
     # Print all unique num pages
@@ -72,6 +56,58 @@ def main():
     print(", ".join(sorted(map(str, num_pages_set))))
 
     print("=" * 110)
+
+def filter_remove_none(filters):
+    if not filters:
+        return None
+
+    result = {}
+    for key, value in filters.items():
+        if value is not None:
+            result[key] = value
+
+    return result
+
+def get_reasoning(query, payload):
+    response = requests.post(reason_query_url, json=payload, headers=headers)
+
+    if not response.ok:
+        print(f"Error {response.status_code}: {response.text}")
+        quit()
+
+    reasoning = response.json()
+    # filters = filter_remove_none(reasoning["filters"])
+    # reasoning["filters"] = filters
+
+    return reasoning
+
+def main():
+    prompts = [
+        "a book about forgiveness",
+        "a 300 page or more books by Stephen King with a sad tone and take place in Maine",
+        "get me books by Stephen King and J.K. Rowling"
+    ]
+
+    # If a query is passed as an argument, use it; otherwise use default
+    if len(sys.argv) > 1:
+        query = " ".join(sys.argv[1:])
+    else:
+        query = prompts[2]
+
+    payload = {"description": query}
+    reasoning = get_reasoning(query, payload)
+    # print(f"REASONING: \n{reasoning}")
+
+    recommend_book_payload = {**payload, **reasoning}
+    response = requests.post(recommend_books_url, json=recommend_book_payload, headers=headers)
+
+    # there is nothing here so we just quit
+    if not response.ok:
+        print(f"Error {response.status_code}: {response.text}")
+        quit()
+
+    result = response.json()
+    print_result(result, query)
 
 if __name__ == "__main__":
     main()
