d7444ee6b00cf2b712799a93df1c1a6cc4ecee4f
Tuan Pham <tuanqpham0921@gmail.com>
2025-08-15 11:01:30 -0500

    remove the star for dev in CORS

--- app/config.py
+++ app/config.py
@@ -23,8 +23,7 @@ def add_cors_middleware(app):
         CORSMiddleware,
         allow_origins=[
             "https://tuanqpham0921.com",
-            "https://www.tuanqpham0921.com",
-            "*" # for DEV
+            "https://www.tuanqpham0921.com"
         ],
         allow_credentials=True,
         allow_methods=["*"],
